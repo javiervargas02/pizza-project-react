cd1c9034062cb698291ba4fa20f40c2c311b9d43
javiervargas02 <javier.v.021204@gmail.com>
2023-11-21 11:19:29 -0600

    Add conditional rendering for sold out pizzas

--- src/components/Pizza/Pizza.js
+++ src/components/Pizza/Pizza.js
@@ -1,11 +1,11 @@
 export default function Pizza({ pizzaData }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaData.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaData.photoName} alt={`Pizza ${pizzaData.name}`} />
       <div>
         <h3> {`Pizza ${pizzaData.name}`} </h3>
         <p>{pizzaData.ingredients}</p>
-        <span>${pizzaData.price}</span>
+        <span>{pizzaData.soldOut ? "SOLD OUT" : pizzaData.price}</span>
       </div>
     </li>
   );
